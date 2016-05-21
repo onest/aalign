0586c0ecab694782f40889925d44bcfeec38f85f
Kaixi <kaixihou@vt.edu>
2016-05-21 14:56:19 -0400

    license headers added

--- include/OptimalStringAlignmentDistance_CPU.h
+++ include/OptimalStringAlignmentDistance_CPU.h
@@ -1,3 +1,25 @@
+/*
+* (c) 2015 Virginia Polytechnic Institute & State University (Virginia Tech)   
+*                                                                              
+*   This program is free software: you can redistribute it and/or modify       
+*   it under the terms of the GNU General Public License as published by       
+*   the Free Software Foundation, either version 3 of the License, or          
+*   (at your option) any later version.                                        
+*                                                                              
+*   This program is distributed in the hope that it will be useful,            
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of             
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              
+*   GNU General Public License for more details.                               
+*                                                                              
+*   You should have received a copy of the GNU General Public License          
+*                                                                              
+*  Acknowledgement                                                             
+*   This research was supported in part by NSF-BIGDATA program via             
+*   IIS-1247693                                                                
+*                                                                              
+*/
+
+
 #ifndef _OPT_STR_ALIGN_DISTANCE_CPU_H
 #define _OPT_STR_ALIGN_DISTANCE_CPU_H
 
